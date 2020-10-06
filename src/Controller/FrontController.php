0ed2b81cab3c8290b1fd83c76688ad1d9b1bcb3a
extasy44 <seoheejun@gmail.com>
2020-10-06 23:11:57 +1100

    added html templates for all pages, added frontController, adminController

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -16,4 +16,60 @@ class FrontController extends AbstractController
             'controller_name' => 'FrontController',
         ]);
     }
+
+    /**
+     * @Route("/video-list", name="video_list")
+     */
+    public function videoList()
+    {
+        return $this->render('front/video_list.html.twig');
+    }
+
+     /**
+     * @Route("/video-details", name="video_details")
+     */
+    public function videoDetails()
+    {
+        return $this->render('front/video_details.html.twig');
+    }
+
+     /**
+     * @Route("/search-results", methods={"POST"}, name="search_results")
+     */
+    public function searchResult()
+    {
+        return $this->render('front/search_results.html.twig');
+    }
+
+     /**
+     * @Route("/pricing", name="pricing")
+     */
+    public function pricing()
+    {
+        return $this->render('front/pricing.html.twig');
+    }
+
+    /**
+     * @Route("/register", name="register")
+     */
+    public function register()
+    {
+        return $this->render('front/register.html.twig');
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login()
+    {
+        return $this->render('front/login.html.twig');
+    }
+
+    /**
+     * @Route("/payment", name="payment")
+     */
+    public function payment()
+    {
+        return $this->render('front/payment.html.twig');
+    }
 }
